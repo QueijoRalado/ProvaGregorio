89ce7294480bf34b0716c62a03ac742d630dfca2
Felipe Gonçalves de Lima <felipe.gdelima10@gmail.com>
2025-04-10 21:05:26 -0300

    meu Deus do ceu

--- 1963.py
+++ 1963.py
@@ -1,5 +1,26 @@
 valor = input("escreva filho: ")
+resultado=0
+lista = (valor.split())
 
-lista = valor.split()
+um_por_cento = int(lista[0])/100
+objetivo = int(lista[1])
 
-print ((20/100)*30)
+modificador = 1
+
+while resultado!=objetivo:
+    resultado = 0.2*modificador
+    modificador+=1
+    print(resultado)
+
+print(modificador)
+"""
+while resultado!=lista[1]:
+    print(resultado)
+    resultado=resultado*um_por_cento
+    um_por_cento = um_por_cento*2
+
+for n in range(0, 100):
+    resultado = 0.1*modificador
+    modificador+=1
+    print(resultado)
+"""
